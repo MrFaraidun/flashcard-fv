6ec3f60b63f9b297458926549a362c0f9c8a19a6
MrFaraidun <faraidunbahaden32@gmail.com>
2025-09-24 21:56:49 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  password: "talk_kurdish",
+  password: "password",
   host: "localhost",
   database: "flashcard",
   port: 5432,
